6da60e4043a0cf5a5d0439d11521b2df3871805f
redig <luizredig@gmail.com>
2024-12-10 18:01:45 -0300

    🧩 feat(components): add BarbershopCard component

---
+++ app/components/barbershop/card/BarbershopCard.tsx
@@ -0,0 +1,43 @@
+import { Barbershop } from "@prisma/client";
+import Image from "next/image";
+import { Button } from "../../ui/button";
+import { Card, CardContent } from "../../ui/card";
+
+type BarbershopCardProps = {
+  barbershop: Barbershop;
+};
+
+const BarbershopCard = ({ barbershop }: BarbershopCardProps) => {
+  return (
+    <>
+      <Card className="h-72 w-40 min-w-40 max-w-40 rounded-2xl">
+        <CardContent className="flex h-full w-full flex-col p-0">
+          <div className="max-h-1/2 flex h-1/2">
+            <Image
+              src={barbershop.imageUrl}
+              width={0}
+              height={0}
+              alt={barbershop.name}
+              className="min-h-full w-full rounded-3xl object-cover p-1"
+              sizes="100vw"
+            />
+          </div>
+
+          <div className="min-h-1/2 g flex h-1/2 flex-col justify-between p-3 pt-0">
+            <div className="flex flex-col gap-2">
+              <span className="truncate text-lg">{barbershop.name}</span>
+
+              <span className="text-sm text-muted-foreground">
+                {barbershop.address}
+              </span>
+            </div>
+
+            <Button className="bg-muted font-semibold">Book</Button>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default BarbershopCard;
