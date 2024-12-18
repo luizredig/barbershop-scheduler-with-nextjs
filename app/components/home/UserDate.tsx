97d9a7d56dd28da2669a42862a62e06d5b574172
redig <luizredig@gmail.com>
2024-12-18 18:30:04 -0300

    🌎 feat(user-date): translate to English

--- app/components/home/UserDate.tsx
+++ app/components/home/UserDate.tsx
@@ -3,18 +3,15 @@ import React from "react";
 
 const UserDate = () => {
   const today = new Date();
-  let formattedDate = today.toLocaleDateString("pt-BR", {
+  const formattedDate = today.toLocaleDateString("en-US", {
     weekday: "long",
     day: "numeric",
     month: "long",
   });
 
-  formattedDate = formattedDate.replace("-feira", "");
-
   return (
     <div className="flex flex-col gap-1">
-      <span className="text-2xl">Olá!</span>
-
+      <span className="text-2xl">Hello!</span>
       <span className="text-md">{capitalize(formattedDate)}</span>
     </div>
   );
